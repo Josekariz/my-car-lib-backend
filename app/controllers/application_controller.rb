5bd473f6144986df7196096c05090e1ecd248e1e
Josekariz <joseph.macharia@student.moringaschool.com>
2022-12-03 12:24:06 +0300

    define iherited authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  def authorize
+    user= User.find_by(id: session[:user_id])
+    render json: {errors: ["Not authorized"]}, status: :unauthorized
+  end
 end
