2a531f2661814f40a2a3e1b1ddcacd11c033a9e3
Josekariz <joseph.macharia@student.moringaschool.com>
2022-12-03 12:28:59 +0300

    edit routes for users resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :reviews
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "session#create"
+  delete "/logout", to: "session#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
